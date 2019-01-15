545183d8cf3bb28d4f1f5c1e505f704bf8e9b9c8
HankMadden <46724260+HankMadden@users.noreply.github.com>
2019-01-15 13:47:05 -0500

    Add files via upload
    Lab 1 Java code for Hank Madden

---
+++ Hello.java
@@ -0,0 +1,24 @@
+/************************
+ * HankM_Java1
+ ************************
+ * Initial Program
+ _______________________
+ * Student: Hank Madden
+ * 1/15/2019
+ * CMSC 255-002
+ ************************/
+ public class Hello {
+   public static void printHeading() {
+     System.out.println("Hank Madden");
+	 System.out.println("Java Project 1");
+	 System.out.println("Test of project");
+	 System.out.println("1/15/2019");
+	 System.out.println("CSMC255-002");
+	 System.out.println();
+}
+
+public static void main(String [] args) {
+   printHeading();
+   System.out.println("Hello");
+   }
+}
